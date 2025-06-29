f48ef4ad1c9340402a931b389a9b23d448feb317
sm299 <shreya87.sm@gmail.com>
2025-06-29 22:11:16 +0530

    backtrack recursion

---
+++ BacktrackRecursion.java
@@ -0,0 +1,16 @@
+public class BacktrackRecursion {
+    public static void main(String args[]) {
+        int n = 5;
+        recur(1, n);
+    }
+
+    public static void recur(int i, int n) {
+        if (i > n) {
+            return;
+        }
+
+        recur(i + 1, n);
+        System.out.print(" " + i + " ");
+
+    }
+}
---
+++ NextPermutation.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NextPermutation {
+    public static void main(String[] args) {
+
+        int[] nums = { 1, 2, 3 };
+        List<Integer> list = new ArrayList<>();
+        for (Integer a : nums) {
+            list.add(a);
+        }
+    }
+
+}
