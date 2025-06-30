0c5fe9198cec11ef393b671b96488503f765073b
sm299 <shreya87.sm@gmail.com>
2025-06-30 19:51:24 +0530

    factorial recursion

---
+++ Factorialrecursion.java
@@ -0,0 +1,14 @@
+public class Factorialrecursion {
+    public static void main(String args[]) {
+        int n = 5;
+        System.out.println(fact(n));
+    }
+
+    public static int fact(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return (n * fact(n - 1));
+        }
+    }
+}
